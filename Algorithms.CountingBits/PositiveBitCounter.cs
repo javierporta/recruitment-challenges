aadde1ae26a85e57de57944962fc213fc192165e
Javier <2190271@my.ipleiria.pt>
2020-08-10 10:32:44 +0200

    Implement Count method

--- Algorithms.CountingBits/PositiveBitCounter.cs
+++ Algorithms.CountingBits/PositiveBitCounter.cs
@@ -6,12 +6,52 @@ namespace Algorithms.CountingBits
 {
     using System;
     using System.Collections.Generic;
+    using System.Runtime.Serialization;
 
     public class PositiveBitCounter
     {
         public IEnumerable<int> Count(int input)
         {
-            throw new NotImplementedException();
+            //Analyze edge ases
+            if (input < 0)
+            {
+                throw new ArgumentException();
+            } else if(input == 0)
+            {
+                return new List<int> { 0 };
+            }
+
+            var binary = Convert.ToString(input, 2);
+            var reversedBinaryArray = ReverseStringToArray(binary);
+
+            var countOfOnes = 0;
+            var result = new List<int>();
+            //NOte: As performance is important, I prefered to use my own loop instead of trusting linq methods
+            for (int index = 0; index < reversedBinaryArray.Length; index++)
+            {
+                if(reversedBinaryArray[index] == '1') //comparing only chars (faster than comapring strings)
+                {
+                    result.Add(index);
+                    countOfOnes++;
+                }
+            }
+
+            //Add count at the beginning of the list
+            result.Insert(0, countOfOnes);
+
+            return  result;
         }
+
+        private char[] ReverseStringToArray(string s)
+        {
+            char[] array = new char[s.Length];
+            int forward = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                array[forward++] = s[i];
+            }
+            return array;
+        }
+
     }
 }
