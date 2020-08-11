ab1d5f84710ac437bb2dbd4003ad1e06cb5bc3e2
Javier <2190271@my.ipleiria.pt>
2020-08-11 10:40:23 +0200

    Decoupled email helper

--- Refactoring.FraudDetection/Common/EmailHelper.cs
+++ Refactoring.FraudDetection/Common/EmailHelper.cs
@@ -9,7 +9,7 @@ namespace Refactoring.FraudDetection.Common
         const char AT_CHAR = '@';
         public static string RemoveStringsFromEmail(string email, string[] stringsToRemove)
         {
-            var splittedEmail = email.Split(new char[] { AT_CHAR }, StringSplitOptions.RemoveEmptyEntries);
+            var splittedEmail = SplitEmail(email);
             var mailbox = splittedEmail[0];
             var domain = splittedEmail[1];
 
@@ -18,13 +18,13 @@ namespace Refactoring.FraudDetection.Common
                 mailbox = mailbox.Replace(stringToRemove, string.Empty);
             }
 
-            var result = string.Join(AT_CHAR, new string[] { mailbox, domain });
+            var result = JoinEmail(mailbox, domain);
             return result;
         }
 
         public static string RemoveSubstringAfterDelimiterFromEmail(string email, string delimiter)
         {
-            var splittedEmail = email.Split(new char[] { AT_CHAR }, StringSplitOptions.RemoveEmptyEntries);
+            var splittedEmail = SplitEmail(email);
 
             var mailbox = splittedEmail[0];
             var domain = splittedEmail[1];
@@ -37,8 +37,17 @@ namespace Refactoring.FraudDetection.Common
                 mailbox = mailbox.Remove(atIndex);
             }
 
-            var result = string.Join(AT_CHAR, new string[] { mailbox, domain });
+            var result = JoinEmail(mailbox, domain);
             return result;
+        }
+
+        private static string[] SplitEmail(string email)
+        {
+            return email.Split(new char[] { AT_CHAR }, StringSplitOptions.RemoveEmptyEntries);
+        }
+        private static string JoinEmail(string mailbox, string domain )
+        {
+            return string.Join(AT_CHAR, new string[] { mailbox, domain });
         }
     }
 }
